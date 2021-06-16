2331e19984b5e3e22bddfb01635c7d1c5d570979
James Fisher <jfisher396@hotmail.com>
2021-06-16 13:41:23 -0700

    added sticky social bar

--- script.js
+++ script.js
@@ -1,44 +1,17 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+// const navBtn = $(".nav-toggle");
 
-
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
 });
 
-
-//grabs the user inputs from the form and sends them to the email route
-$("#form").on('submit', function(e){
-    e.preventDefault();
-        
-    let newEmail = {
-        name: $("#name").val().trim(),
-        subject: $("#subject").val().trim(),
-        company: $("#company").val().trim(),
-        email: $("#email").val().trim(),
-        message: $("#message").val().trim()
-    }
-    //renders an alert stating that email was sent successfully
-    $.post("/contact", newEmail)
-        .then(function(data){
-            
-            $("#name").val("")
-            $("#subject").val("")
-            $("#company").val("")
-            $("#email").val("")
-            $("#message").val("")
-
-            if(data) {
-                $("#email-alert").text("Email Sent Successfully").addClass("email-success").fadeOut(5000)
-            } 
-        })
-        
-});
-
+// navBtn.on("click", function (e) {
+//   $("body").toggleClass("overflow-hidden");
+// });
